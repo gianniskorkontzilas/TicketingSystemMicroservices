8d545b5f7099f0c70555ae0d48dedb123a6e1ad5
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-06-18 20:18:42 +0300

    implements OrderService

---
+++ orderservice/src/main/java/com/ticketsystem/orderservice/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.ticketsystem.orderservice.service;
+
+import com.ticketsystem.bookingservice.event.BookingEvent;
+import com.ticketsystem.orderservice.client.InventoryServiceClient;
+import com.ticketsystem.orderservice.entity.Order;
+import com.ticketsystem.orderservice.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderService {
+
+    private OrderRepository orderRepository;
+    private InventoryServiceClient inventoryServiceClient;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository,
+                        InventoryServiceClient inventoryServiceClient) {
+        this.orderRepository = orderRepository;
+        this.inventoryServiceClient = inventoryServiceClient;
+    }
+
+    @KafkaListener(topics = "booking", groupId = "order-service")
+    public void orderEvent(BookingEvent bookingEvent) {
+        log.info("Received order event: {}", bookingEvent);
+        Order order = createOrder(bookingEvent);
+        orderRepository.saveAndFlush(order);
+
+        inventoryServiceClient.updateInventory(order.getEventId(), order.getTicketCount());
+        log.info("Inventory updated for event: {}, less tickets: {}", order.getEventId(), order.getTicketCount());
+    }
+
+
+    private Order createOrder(BookingEvent bookingEvent) {
+        return Order.builder()
+                .customerId(bookingEvent.getUserId())
+                .eventId(bookingEvent.getEventId())
+                .ticketCount(bookingEvent.getTicketCount())
+                .totalPrice(bookingEvent.getTotalPrice())
+                .build();
+    }
+}
